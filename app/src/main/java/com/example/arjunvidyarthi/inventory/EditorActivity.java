9bba9e7dd47d6952b898e4ad2aace90590457db2
Arjun Vidyarthi <aleronarjun@gmail.com>
2017-11-19 01:59:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/arjunvidyarthi/inventory/EditorActivity.java
@@ -0,0 +1,19 @@
+package com.example.arjunvidyarthi.inventory;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Arjun Vidyarthi on 19-Nov-17.
+ */
+
+public class EditorActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
+
+    }
+
+}
