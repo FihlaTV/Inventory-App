9f25a3be609481bdd387df9cc41637e32acbaf11
Arjun Vidyarthi <aleronarjun@gmail.com>
2017-11-18 14:30:18 +0530

    Initial commit

--- app/src/main/java/com/example/arjunvidyarthi/inventory/CatalogActivity.java
+++ app/src/main/java/com/example/arjunvidyarthi/inventory/CatalogActivity.java
@@ -9,6 +9,5 @@ public class CatalogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
-
     }
 }
